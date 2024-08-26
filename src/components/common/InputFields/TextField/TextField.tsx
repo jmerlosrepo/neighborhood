98a6f345d143fc4dacca50f2782f387442276a3e
jemega1545 <jmerlosrepos@outlook.com>
2024-08-26 16:37:15 -0600

    Text Field added

--- src/components/common/InputFields/TextField/TextField.tsx
+++ src/components/common/InputFields/TextField/TextField.tsx
@@ -1,5 +1,63 @@
-const TextField = () => {
-  return <div>TextField</div>;
+import { useState, useEffect } from "react";
+import classNames from "classnames";
+
+import { TextFieldProps } from "../../../../types";
+import { textInputType } from "../../../../enums";
+
+import styles from "./TextField.module.scss";
+
+const TextField = ({
+  id,
+  name,
+  inputType,
+  value,
+  label,
+  isShowClearText,
+  className,
+}: TextFieldProps) => {
+  const [inputValue, setInputVale] = useState<string>(value);
+  const [showText, setShowText] = useState<boolean>(false);
+
+  useEffect(() => {
+    setInputVale(value);
+  }, [value]);
+
+  useEffect(() => {
+    setShowText(isShowClearText);
+  }, [isShowClearText]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputVale(e.target.value);
+  };
+
+  return (
+    <div
+      className={classNames(styles.defaultInputStyle, {
+        [className]: className,
+      })}
+    >
+      <label htmlFor={id}>
+        {label}
+        <input
+          type={inputType}
+          id={id}
+          name={name}
+          value={inputValue}
+          onChange={handleChange}
+        />
+      </label>
+      {inputType === textInputType.password && (
+        <input
+          className={classNames({
+            [styles.passwordCheckboxOn]: showText,
+            [styles.passwordCheckboxOff]: !showText,
+          })}
+          type="checkbox"
+          checked={showText}
+        />
+      )}
+    </div>
+  );
 };
 
 export default TextField;
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,5 @@
+import { textInputType } from "./enums";
+
 export type ButtonProps = {
   className: string;
   label: string;
@@ -8,4 +10,14 @@ export type FormContainerProps = {
   children: React.ReactNode;
   header: string;
   onFormSubmit: Function;
+};
+
+export type TextFieldProps = {
+  id: string;
+  name: string;
+  label: string;
+  className: string;
+  inputType: textInputType;
+  value: string;
+  isShowClearText: boolean;
 };
